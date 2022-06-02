ff0134904e3a0ae65921cb3aa173ce8d14ab3b91
ignatievpeter <itbecomesdarkveryfasthere@gmail.com>
2022-06-03 01:51:00 +0300

    navigation with numbers

--- js/script.js
+++ js/script.js
@@ -11,8 +11,7 @@ function showImage(i) {
     let images = document.querySelectorAll('.image')
     let nav = document.querySelectorAll('.pic')
 
-    for (i = 0; i < images.length; i++)
-    {
+    for (i = 0; i < images.length; i++) {
         images[i].style.display = 'none'
     }
 
@@ -20,38 +19,37 @@ function showImage(i) {
         nav[i].className = nav[i].className.replace(" active", "")
     }
 
-    if (index > images.length -1) index = 0
+    if (index > images.length - 1) index = 0
 
-    if (index < 0) index = images.length -1
-    console.log("images length", images.length, )
+    if (index < 0) index = images.length - 1
+    console.log("images length", images.length,)
     console.log(index)
     images[index].style.display = 'block'
     nav[index].classList.add('active')
 }
 
 showImage(1)
-rightButton.addEventListener("click", function(){ showImage(1); })
-leftButton.addEventListener("click", function(){ showImage(-1); })
-// numbers.addEventListener('click', ()=>{
-//     index = numbers.;
-//     console.log(numbers.textContent, index)
-//     let images = document.querySelectorAll('.image')
-//     let nav = document.querySelectorAll('.pic')
-//
-//     for (let i = 0; i < images.length; i++)
-//     {
-//         images[i].style.display = 'none'
-//     }
-//
-//     for (let i = 0; i < nav.length; i++) {
-//         nav[i].className = nav[i].className.replace(" active", "")
-//     }
-//
-//     if (index > images.length -1) index = 0
-//
-//     if (index < 0) index = images.length -1
-//     console.log("images length", images.length, )
-//
-//     images[index].style.display = 'block'
-//     nav[index].classList.add('active')
-// })
+rightButton.addEventListener("click", function () {
+    showImage(1);
+})
+leftButton.addEventListener("click", function () {
+    showImage(-1);
+})
+
+for (let i = 0; i < numbers.length; i++) {
+
+    numbers[i].addEventListener('click', function () {
+        let images = document.querySelectorAll('.image')
+        let nav = document.querySelectorAll('.pic')
+
+        for (let j= 0; j < images.length; j++) {
+            images[j].style.display = 'none'
+        }
+        for (let j = 0; j < nav.length; j++) {
+            nav[j].className = nav[j].className.replace(" active", "")
+        }
+
+        images[i].style.display = 'block'
+        nav[i].classList.add('active')
+    })
+}
